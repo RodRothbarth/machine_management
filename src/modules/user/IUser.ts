1a2bbc6ec2728c6991d860ccc37e89df004e9d25
rodrothbarth <rodrothbarth@gmail.com>
2024-09-24 09:47:48 -0300

    new folder for frontend

--- src/modules/user/IUser.ts
+++
@@ -1,9 +0,0 @@
-import { Types } from "mongoose";
-
-export interface IUser {
-  _id?: string;
-  name: string;
-  password: string;
-  email: string;
-  profile: Types.ObjectId;
-}
--- src/shared/errorHandling/catchAsync.ts
+++
@@ -1,8 +0,0 @@
-import { NextFunction, Request, Response } from "express";
-
-// handling try/catch situations
-export const catchAsync = (fn: any) => {
-  return (req: Request, res: Response, next: NextFunction): void => {
-    fn(req, res, next).catch(next);
-  };
-};
